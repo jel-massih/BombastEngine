7c4b988d66b373cfda582c9ed8ece2fcc5bf995b
Jason El-Massih <jel-massih@hotmail.com>
2014-06-01 14:42:16 -0400

    Added Model Class for models.

---
+++ Source/Graphics3D/ModelClass.cpp
@@ -0,0 +1,183 @@
+#include "ModelClass.h"
+
+ModelClass::ModelClass()
+{
+	m_pVertexBuffer = 0;
+	m_pIndexBuffer = 0;
+}
+
+ModelClass::ModelClass(const ModelClass& other)
+{
+}
+
+ModelClass::~ModelClass()
+{
+}
+
+bool ModelClass::Initialize(ID3D11Device* device, WCHAR* textureFilepath)
+{
+	bool result;
+
+	result = InitializeBuffers(device);
+	if (!result)
+	{
+		return false;
+	}
+
+	result = LoadTexture(device, textureFilepath);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ModelClass::Shutdown()
+{
+	ReleaseTexture();
+	ShutdownBuffers();
+}
+
+void ModelClass::Render(ID3D11DeviceContext* context)
+{
+	RenderBuffers(context);
+}
+
+int ModelClass::GetIndexCount()
+{
+	return m_indexCount;
+}
+
+ID3D11ShaderResourceView* ModelClass::GetTexture()
+{
+	return m_pTexture->GetTexture();
+}
+
+bool ModelClass::InitializeBuffers(ID3D11Device* device)
+{
+	VertexType* vertices;
+	unsigned long* indices;
+	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+
+	m_vertexCount = 3;
+	m_indexCount = 3;
+
+	vertices = new VertexType[m_vertexCount];
+	if (!vertices)
+	{
+		return false;
+	}
+
+	indices = new unsigned long[m_indexCount];
+	if (!indices)
+	{
+		return false;
+	}
+
+	vertices[0].position = DirectX::XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f);
+	vertices[0].texture = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	vertices[1].position = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	vertices[1].texture = DirectX::XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f);
+
+	vertices[2].position = DirectX::XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f);
+	vertices[2].texture = DirectX::XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f);
+
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 2;
+
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType)* m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_pVertexBuffer);
+	if (FAILED(result)) 
+	{
+		return false;
+	}
+
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_pIndexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	SAFE_DELETE_ARRAY(vertices);
+	SAFE_DELETE_ARRAY(indices);
+
+	return true;
+}
+
+void ModelClass::ShutdownBuffers()
+{
+	SAFE_RELEASE(m_pIndexBuffer);
+	SAFE_RELEASE(m_pVertexBuffer);
+}
+
+void ModelClass::RenderBuffers(ID3D11DeviceContext* context)
+{
+	unsigned int stride;
+	unsigned int offset;
+
+	stride = sizeof(VertexType);
+	offset = 0;
+
+	context->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+
+	context->IASetIndexBuffer(m_pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+}
+
+bool ModelClass::LoadTexture(ID3D11Device* device, WCHAR* textureFilepath)
+{
+	bool result;
+	ID3D11DeviceContext* context;
+
+	m_pTexture = new TextureClass;
+	if (!m_pTexture)
+	{
+		return false;
+	}
+
+	device->GetImmediateContext(&context);
+
+	result = m_pTexture->Initialize(device, context, textureFilepath);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ModelClass::ReleaseTexture()
+{
+	if (m_pTexture)
+	{
+		m_pTexture->Shutdown();
+		SAFE_DELETE(m_pTexture);
+	}
+}
---
+++ Source/Graphics3D/ModelClass.h
@@ -0,0 +1,43 @@
+#ifndef MODEL_CLASS_H
+#define MODEL_CLASS_H
+
+#include "../BombastEngineStd.h"
+#include "../Graphics2D/TextureClass.h"
+
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		DirectX::XMVECTOR position;
+		DirectX::XMVECTOR texture;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass& other);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device* device, WCHAR* textureFilepath);
+	void Shutdown();
+	void Render(ID3D11DeviceContext* context);
+
+	int GetIndexCount();
+
+	ID3D11ShaderResourceView* GetTexture();
+
+private:
+	bool InitializeBuffers(ID3D11Device* device);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext* context);
+
+	bool LoadTexture(ID3D11Device* device, WCHAR* textureFilepath);
+	void ReleaseTexture();
+
+private:
+	ID3D11Buffer* m_pVertexBuffer, *m_pIndexBuffer;
+	int m_vertexCount, m_indexCount;
+	TextureClass* m_pTexture;
+};
+
+#endif
