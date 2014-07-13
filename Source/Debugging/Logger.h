e17ae14033f8ceb8335063400b13d697fad7b66a
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 04:39:20 -0400

    Logger uses wstring for easier throughout application

--- Source/Debugging/Logger.h
+++ Source/Debugging/Logger.h
@@ -1,8 +1,16 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <string>
+
 // Call the BE_ERROR() macro to send out an error message
 //		Example -> BE_ERROR("Something Blew Up!");
-#define BE_ERROR(str) if(BombastApp::GetGameInstance()->GetHwnd() != NULL) {MessageBox(BombastApp::GetGameInstance()->GetHwnd(), str, L"Error", MB_OK);} else {OutputDebugString(str);}
+#define BE_ERROR(str) \
+	std::wstring s((str)); \
+	if(BombastApp::GetGameInstance()->GetHwnd() != NULL) { \
+		MessageBox(BombastApp::GetGameInstance()->GetHwnd(), s.c_str(), L"Error", MB_OK); \
+	} else { \
+		OutputDebugString(s.c_str()); \
+	} \
 
 #endif
