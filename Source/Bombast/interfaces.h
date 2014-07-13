be813aec60c545fb08b9ce0a8e85f077250c839b
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 12:03:17 -0400

    Addeed some Resource Classes to interfaces.h

--- Source/Bombast/interfaces.h
+++ Source/Bombast/interfaces.h
@@ -22,4 +22,30 @@ public:
 	virtual void VChangeState(enum CoreGameState newState) = 0;
 };
 
+
+class ResourceHandle;
+class Resource;
+
+class IResourceLoader
+{
+public:
+	virtual std::string VGetPattern() = 0;
+	virtual bool VUseRawFile() = 0;
+	virtual bool VDiscardRawBufferAfterLoad() = 0;
+	virtual bool VAddNullZero() { return false; }
+	virtual unsigned int VGetLoadedResourceSize(char *rawBuffer, unsigned int rawSize) = 0;
+	virtual bool VLoadResource(char *rawBuffer, unsigned int rawSize, ResourceHandle* handle) = 0;
+};
+
+class IResourceFile
+{
+public:
+	virtual bool VOpen() = 0;
+	virtual int VGetRawResourceSize(const Resource &r) = 0;
+	virtual int VGetRawResource(const Resource &r, char *buffer) = 0;
+	virtual int VGetNumResources() const = 0;
+	virtual std::string VGetResourceName(int num) const = 0;
+	virtual ~IResourceFile() { }
+};
+
 #endif
