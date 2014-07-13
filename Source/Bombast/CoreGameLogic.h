9a5fb2f5f11198ae825577c91871e08704fb375e
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 03:50:32 -0400

    Added Core game logic header

---
+++ Source/Bombast/CoreGameLogic.h
@@ -0,0 +1,78 @@
+#ifndef CORE_GAME_LOGIC_H
+#define CORE_GAME_LOGIC_H
+
+#include "../BombastEngineStd.h"
+#include "interfaces.h"
+#include <map>
+#include <vector>
+
+class ActorFactory;
+class LevelManager;
+
+enum CoreGameState
+{
+	CGS_Invalid,
+	CGS_Initializing,
+	CGS_MainMenu,
+	CGS_LoadingGameEnvironment,
+	CGS_SpawningPlayersActors,
+	CGS_Running
+};
+
+typedef std::map<ActorId, Actor*> ActorMap;
+typedef std::string Level;
+
+class LevelManager
+{
+public:
+	const std::vector<Level> &GetLevels() const { return m_levels; }
+	const int GetCurrentLevel() const { return m_currentLevel; }
+	bool Initialize(std::vector<std::string> &levels);
+
+protected:
+	std::vector<Level> m_levels;
+	int m_currentLevel;
+};
+
+class CoreGameLogic : public IGameLogic
+{
+public:
+	CoreGameLogic();
+	virtual ~CoreGameLogic();
+
+	bool Initialize();
+	
+	ActorId GetNewActorId()
+	{
+		return ++m_lastActorId;
+	}
+
+	virtual Actor* VCreateActor(const std::string &actorResource, rapidxml::xml_node<> *overrides);
+	virtual void VDestroyActor(const ActorId actorId);
+	virtual Actor* VGetActor(const ActorId actorId);
+
+	std::string GetActorXml(const ActorId id);
+
+	const LevelManager* GetLevelManager() { return m_pLevelManager; }
+	virtual bool VLoadGame(const char* levelResource) override;
+
+	virtual void VChangeState(CoreGameState newState);
+	const CoreGameState GetState() const { return m_gameState; }
+	
+
+protected:
+	float m_lifetime; //How long game has been active
+	ActorMap m_actors;
+	ActorId m_lastActorId;
+	CoreGameState m_gameState;
+
+	ActorFactory* m_pActorFactory;
+
+	LevelManager* m_pLevelManager;
+
+	virtual ActorFactory* VCreateActorFactory();
+
+	virtual bool VLoadGameDelegate(rapidxml::xml_node<>* pLevelData) { return true; }
+};
+
+#endif
--- Source/Bombast/interfaces.h
+++ Source/Bombast/interfaces.h
@@ -1,6 +1,8 @@
 #ifndef INTERFACES_H
 #define INTERFACES_H
 
+#include "..\Utilities\rapidxml.hpp"
+
 class Actor;
 
 typedef unsigned int ActorId;
@@ -8,5 +10,15 @@ typedef unsigned int ComponentId;
 
 const ActorId INVALID_ACTOR_ID = 0;
 const ComponentId INVALID_COMPONENT_ID = 0;
+
+class IGameLogic
+{
+public:
+	virtual Actor* VGetActor(const ActorId id) = 0;
+	virtual Actor* VCreateActor(const std::string &actorResource, rapidxml::xml_node<> *overrides) = 0;
+	virtual void VDestroyActor(const ActorId actorId) = 0;
+	virtual bool VLoadGame(const char* levelResource) = 0;
+	virtual void VChangeState(enum CoreGameState newState) = 0;
+};
 
 #endif
