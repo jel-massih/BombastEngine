485f9bc02427593be8e8dffebc3e2ee3465f165e
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 19:58:47 -0400

    Added ResourceZipFile Definition to resourcecache file

--- Source/Resources/ResourceCache.cpp
+++ Source/Resources/ResourceCache.cpp
@@ -9,6 +9,61 @@ Resource::Resource(const std::string& name)
 	std::transform(m_name.begin(), m_name.end(), m_name.begin(), (int(*)(int)) std::tolower);
 }
 
+ResourceZipFile::~ResourceZipFile()
+{
+	SAFE_DELETE(m_pZipFile);
+}
+
+bool ResourceZipFile::VOpen()
+{
+	m_pZipFile = BE_NEW ZipFile;
+	if (m_pZipFile)
+	{
+		return m_pZipFile->Init(m_resFileName.c_str());
+	}
+	return false;
+}
+
+int ResourceZipFile::VGetRawResourceSize(const Resource &r)
+{
+	int resourceNum = m_pZipFile->Find(r.m_name.c_str());
+	if (resourceNum == -1)
+	{
+		return -1;
+	}
+
+	return m_pZipFile->GetFileLen(resourceNum);
+}
+
+int ResourceZipFile::VGetRawResource(const Resource& r, char* buffer)
+{
+	int size = 0;
+	int resourceNum = m_pZipFile->Find(r.m_name.c_str());
+	if (resourceNum != -1)
+	{
+		size = m_pZipFile->GetFileLen(resourceNum);
+		m_pZipFile->ReadFile(resourceNum, buffer);
+	}
+
+	return size;
+}
+
+int ResourceZipFile::VGetNumResources() const
+{
+	return (m_pZipFile == NULL) ? 0 : m_pZipFile->GetNumFiles();
+}
+
+std::string ResourceZipFile::VGetResourceName(int num) const
+{
+	std::string resName = "";
+	if (m_pZipFile != NULL && num >= 0 && num < m_pZipFile->GetNumFiles())
+	{
+		resName = m_pZipFile->GetFilename(num);
+	}
+
+	return resName;
+}
+
 ResourceHandle::ResourceHandle(Resource& resource, char* buffer, unsigned int size, ResourceCache* pResCache) : m_resource(resource)
 {
 	m_buffer = buffer;
@@ -36,6 +91,13 @@ ResourceCache::~ResourceCache()
 		FreeOneResource();
 	}
 	SAFE_DELETE(m_pFile);
+
+	while (!m_resourceLoaders.empty())
+	{
+		IResourceLoader* resource = *(m_resourceLoaders.begin());
+		SAFE_DELETE(resource);
+		m_resourceLoaders.pop_front();
+	}
 }
 
 bool ResourceCache::Initialize()
