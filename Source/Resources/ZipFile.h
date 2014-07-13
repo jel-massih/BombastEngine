c2e84fd86f571f7cd867c07e9a05b5d90e44d573
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 14:39:30 -0400

    Added ZipFile Class for reading Zip FIles

---
+++ Source/Resources/ZipFile.h
@@ -0,0 +1,52 @@
+#ifndef ZIP_FILE_H
+#define ZIP_FILE_H
+
+// --------------------------------------------------------------------------
+// File:        ZipFile.h
+//
+// Purpose:     The declaration of a quick'n dirty ZIP file reader class.
+//              Original code from Javier Arevalo.
+//              Get zlib from http://www.cdrom.com/pub/infozip/zlib/
+// --------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <map>
+
+typedef std::map<std::string, int> ZipContentsMap;		// maps path to a zip content id
+
+class ZipFile
+{
+public:
+	ZipFile() { m_nEntries = 0; m_pFile = NULL; m_pDirData = NULL; }
+	virtual ~ZipFile() { End(); fclose(m_pFile); }
+
+	bool Init(const std::wstring &resFileName);
+	void End();
+
+	int GetNumFiles()const { return m_nEntries; }
+	std::string GetFilename(int i) const;
+	int GetFileLen(int i) const;
+	bool ReadFile(int i, void *pBuf);
+
+	// Added to show multi-threaded decompression
+	bool ReadLargeFile(int i, void *pBuf, void(*progressCallback)(int, bool &));
+
+	int Find(const std::string &path) const;
+
+	ZipContentsMap m_ZipContentsMap;
+
+private:
+	struct TZipDirHeader;
+	struct TZipDirFileHeader;
+	struct TZipLocalHeader;
+
+	FILE *m_pFile;		// Zip file
+	char *m_pDirData;	// Raw data buffer.
+	int  m_nEntries;	// Number of entries.
+
+	// Pointers to the dir entries in pDirData.
+	const TZipDirFileHeader **m_papDir;
+};
+
+
+#endif
