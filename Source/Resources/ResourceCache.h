99c0ee0b66425b2fdac0dca7292cdeacc60b3719
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 14:40:29 -0400

    Added ResourceZipFile Interface

--- Source/Resources/ResourceCache.h
+++ Source/Resources/ResourceCache.h
@@ -2,6 +2,7 @@
 #define RESOURCE_CACHE_H
 
 #include "../Bombast/interfaces.h"
+#include "ZipFile.h"
 #include <list>
 #include <map>
 #include <vector>
@@ -15,6 +16,24 @@ public:
 	std::string m_name;
 	Resource(const std::string &name);
 };
+
+class ResourceZipFile : public IResourceFile
+{
+	ZipFile *m_pZipFile;
+	std::wstring m_resFileName;
+
+public:
+	ResourceZipFile(const std::wstring resFileName) { m_pZipFile = NULL; m_resFileName = resFileName; }
+	virtual ~ResourceZipFile();
+
+	virtual bool VOpen();
+	virtual int VGetRawResourceSize(const Resource &r);
+	virtual int VGetRawResource(const Resource &r, char *buffer);
+	virtual int VGetNumResources() const;
+	virtual std::string VGetResourceName(int num) const;
+	virtual bool VIsUsingDevelopmentDirectories(void) const { return false; }
+};
+
 
 class ResourceHandle
 {
