c50b7d2dd3166e8370395bb3b9ecc9c2f6346757
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 13:57:47 -0400

    Added WildcardMatch to String utility

--- Source/Utilities/String.h
+++ Source/Utilities/String.h
@@ -4,6 +4,9 @@
 
 #define MAX_DIGITS_IN_INT 12  // max number of digits in an int (-2147483647 = 11 digits, +1 for the '\0')
 
+// Does a classic * & ? pattern match on a file name - this is case sensitive!
+extern BOOL WildcardMatch(const char *pat, const char *str);
+
 extern std::string ToStr(int num, int base = 10);
 extern std::string ToStr(unsigned int num, int base = 10);
 extern std::string ToStr(unsigned long num, int base = 10);
