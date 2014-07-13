63220ee60e032aca7e29a494ba655ef10c221c32
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 05:13:41 -0400

    Added Precompiled Headers to decreaseCompile Time

---
+++ Source/Msvc/stdafx.h
@@ -0,0 +1,9 @@
+#pragma message("Compiling precompiled headers.\n")
+
+#include "../BombastEngineStd.h"
+#include "../Utilities/DDSTextureLoader.h"
+#include "../Utilities/rapidxml.hpp"
+#include "../Utilities/String.h"
+#include "../Utilities/types.h"
+#include "../Utilities/Initialization.h"
+#include "../Graphics3D/D3DClass.h"
