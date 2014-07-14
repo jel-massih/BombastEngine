9362b7c53786808d14951c3a2930d3f4b2da1951
Jason El-Massih <jel-massih@hotmail.com>
2014-07-13 21:35:57 -0400

    Added templates.h utility function

---
+++ Source/Utilities/templates.h
@@ -0,0 +1,42 @@
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+
+//Template Gathered from GameCode Complete
+
+template <class BaseType, class SubType>
+BaseType* GenericObjectCreationFunction(void) { return new SubType; }
+
+template <class BaseClass, class IdType>
+class GenericObjectFactory
+{
+	typedef BaseClass* (*ObjectCreationFunction)(void);
+	std::map<IdType, ObjectCreationFunction> m_creationFunctions;
+
+public:
+	template <class SubClass>
+	bool Register(IdType id)
+	{
+		auto findIt = m_creationFunctions.find(id);
+		if (findIt == m_creationFunctions.end())
+		{
+			m_creationFunctions[id] = &GenericObjectCreationFunction<BaseClass, SubClass>; 
+			return true;
+		}
+
+		return false;
+	}
+
+	BaseClass* Create(IdType id)
+	{
+		auto findIt = m_creationFunctions.find(id);
+		if (findIt != m_creationFunctions.end())
+		{
+			ObjectCreationFunction pFunc = findIt->second;
+			return pFunc();
+		}
+
+		return NULL;
+	}
+};
+
+#endif
