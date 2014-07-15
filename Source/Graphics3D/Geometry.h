d3691bc105d5c139a8ae0e56da6f2141746ffcfd
Jason El-Massih <jel-massih@hotmail.com>
2014-07-15 08:28:53 -0400

    Added Mat4x4 Math helper object

--- Source/Graphics3D/Geometry.h
+++ Source/Graphics3D/Geometry.h
@@ -17,5 +17,154 @@ public:
 	Vec3(const double _x, const double _y, const double _z) { x = (float)_x; y = (float)_y; z = (float)_z; }
 };
 
+extern Vec3 g_Up;
+extern Vec3 g_Right;
+extern Vec3 g_Forward;
+
+class Mat4x4 : public XMFLOAT4X4
+{
+public:
+	inline void SetPosition(Vec3 const &pos);
+	inline void SetScale(Vec3 const &scale);
+
+	inline Vec3 GetPosition() const;
+	inline Vec3 GetDirection() const;
+	inline Vec3 GetUp() const;
+	inline Vec3 GetRight() const;
+	inline Vec3 GetYawPitchRoll() const;
+	inline Vec3 GetScale() const;
+	inline Vec3 Xform(Vec3 &v) const;
+	inline Mat4x4 Inverse() const;
+
+	Mat4x4(XMFLOAT4X4& mat) { memcpy(&m, &mat.m, sizeof(mat.m)); }
+	Mat4x4() : XMFLOAT4X4(){}
+
+	static const Mat4x4 g_Identity;
+
+	inline void BuildTranslation(const Vec3 &pos);
+	inline void BuildTranslation(const float x, const float y, const float z);
+	inline void BuildRotationX(const float radians) { XMStoreFloat4x4(this, XMMatrixRotationX(radians)); }
+	inline void BuildRotationY(const float radians) { XMStoreFloat4x4(this, XMMatrixRotationY(radians)); }
+	inline void BuildRotationZ(const float radians) { XMStoreFloat4x4(this, XMMatrixRotationZ(radians)); }
+	inline void BuildScale(const float x, const float y, const float z);
+};
+
+inline void Mat4x4::SetPosition(Vec3 const &pos)
+{
+	m[3][0] = pos.x;
+	m[3][1] = pos.y;
+	m[3][2] = pos.z;
+	m[3][3] = 1.0f;
+}
+
+inline void Mat4x4::SetScale(Vec3 const &scale)
+{
+	m[1][1] = scale.x;
+	m[2][2] = scale.y;
+	m[3][3] = scale.z;
+}
+
+inline Vec3 Mat4x4::GetPosition() const
+{
+	return Vec3(m[3][0], m[3][1], m[3][2]);
+}
+
+inline Vec3 Mat4x4::GetDirection() const
+{
+	Mat4x4 rot = *this;
+	rot.SetPosition(Vec3(0.f, 0.f, 0.f));
+	Vec3 forward = rot.Xform(g_Forward);
+	return forward;
+}
+
+inline Vec3 Mat4x4::GetRight() const
+{
+	Mat4x4 rot = *this;
+	rot.SetPosition(Vec3(0.f, 0.f, 0.f));
+	Vec3 right = rot.Xform(g_Right);
+	return right;
+}
+
+inline Vec3 Mat4x4::GetUp() const
+{
+	Mat4x4 rot = *this;
+	rot.SetPosition(Vec3(0.f, 0.f, 0.f));
+	Vec3 up = rot.Xform(g_Up);
+	return up;
+}
+
+inline Vec3 Mat4x4::GetYawPitchRoll() const
+{
+	float yaw, pitch, roll;
+
+	pitch = asin(-_32);
+
+	double threshold = 0.001; //UGLY HARDCODE MAGIC NUMBER
+	double test = cos(pitch);
+
+	if (test > threshold)
+	{
+		roll = atan2(_12, _22);
+		yaw = atan2(_31, _33);
+	}
+	else
+	{
+		roll = atan2(-_21, _11);
+		yaw = 0.0;
+	}
+
+	return Vec3(yaw, pitch, roll);
+}
+
+inline Vec3 Mat4x4::GetScale() const
+{
+	return Vec3(m[0][0], m[1][1], m[2][2]);
+}
+
+inline Vec3 Mat4x4::Xform(Vec3& v) const
+{
+	Vec3 temp(v);
+	Vec3 out;
+	XMStoreFloat3(&out, XMVector4Transform(XMLoadFloat3(&temp), XMLoadFloat4x4(this)));
+	return Vec3(out.x, out.y, out.z);
+}
+
+inline Mat4x4 Mat4x4::Inverse() const
+{
+	Mat4x4 out;
+	XMStoreFloat4x4(&out, XMMatrixInverse(NULL, XMLoadFloat4x4(this)));
+	return out;
+}
+
+inline void Mat4x4::BuildTranslation(const Vec3& pos)
+{
+	*this = Mat4x4::g_Identity;
+	m[3][0] = pos.x;
+	m[3][1] = pos.y;
+	m[3][2] = pos.z;
+}
+
+inline void Mat4x4::BuildTranslation(const float x, const float y, const float z)
+{
+	*this = Mat4x4::g_Identity;
+	m[3][0] = x;
+	m[3][1] = y;
+	m[3][2] = z;
+}
+
+inline void Mat4x4::BuildScale(const float x, const float y, const float z)
+{
+	*this = Mat4x4::g_Identity;
+	m[1][1] = x;
+	m[2][2] = y;
+	m[3][3] = z;
+}
+
+inline Mat4x4 operator * (const Mat4x4& a, const Mat4x4& b)
+{
+	Mat4x4 out;
+	XMStoreFloat4x4(&out, XMMatrixMultiply(XMLoadFloat4x4(&a), XMLoadFloat4x4(&b)));
+	return out;
+}
 
 #endif
