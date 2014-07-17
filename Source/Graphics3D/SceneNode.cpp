aa8a9c5fdc8d3618d36e751a7a2bb35591ec3fd1
Jason El-Massih <jel-massih@hotmail.com>
2014-07-17 08:56:58 -0400

    Initial SceneNode setup

--- Source/Graphics3D/SceneNode.cpp
+++ Source/Graphics3D/SceneNode.cpp
@@ -1,4 +1,2 @@
 #include "SceneNode.h"
 #include "../BombastEngineStd.h"
-#include "../Actor/Actor.h"
-#include "Geometry.h"
--- Source/Graphics3D/SceneNode.h
+++ Source/Graphics3D/SceneNode.h
@@ -1,5 +1,36 @@
 #ifndef SCENE_NODE_H
 #define SCENE_NODE_H
 
+#include "../Actor/Actor.h"
+#include "Geometry.h"
+#include "Material.h"
+
+class SceneNode;
+
+//Defines available types of Alpha BLending
+enum AlphaType
+{
+	AlphaOpaque,
+	AlphaTexture,
+	AlphaMaterial,
+	AlphaVertex
+};
+
+//Contained in SceneNode class to provide easy accessor
+class SceneNodeProperties
+{
+	friend class SceneNode;
+public:
+
+protected:
+	ActorId m_actorId;
+	std::string m_name;
+	Mat4x4 m_toWorld, m_fromWorld;
+	float m_radius;
+	RenderPass m_renderPass;
+	Material m_material;
+	AlphaType m_alphaType;
+
+};
 
 #endif
