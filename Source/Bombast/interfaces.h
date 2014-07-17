3411d2901188d214bf7d48a8d59835f4554562e5
Jason El-Massih <jel-massih@hotmail.com>
2014-07-17 08:55:18 -0400

    Added RenderPass enum to interfaces.h

--- Source/Bombast/interfaces.h
+++ Source/Bombast/interfaces.h
@@ -48,4 +48,14 @@ public:
 	virtual ~IResourceFile() { }
 };
 
+enum RenderPass
+{
+	RenderPass_0,
+	RenderPass_Static = RenderPass_0,
+	RenderPass_Actor,
+	RenderPass_Sky,
+	RenderPass_NotRendered,
+	RenderPass_Last
+};
+
 #endif
