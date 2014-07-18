7eaf764268cd70c88557855ae9dc01af19f5cef8
Jason El-Massih <jel-massih@hotmail.com>
2014-07-17 21:48:56 -0400

    Defined rest of scene node

--- Source/Graphics3D/SceneNode.cpp
+++ Source/Graphics3D/SceneNode.cpp
@@ -1,2 +1,23 @@
 #include "SceneNode.h"
 #include "../BombastEngineStd.h"
+
+SceneNodeProperties::SceneNodeProperties()
+{
+	m_actorId = INVALID_ACTOR_ID;
+	m_radius = 0;
+	m_renderPass = RenderPass_0;
+	m_alphaType = AlphaOpaque;
+}
+
+void SceneNodeProperties::Transform(Mat4x4* toWorld, Mat4x4* fromWorld) const
+{
+	if (toWorld)
+	{
+		*toWorld = m_toWorld;
+	}
+	
+	if (fromWorld)
+	{
+		*fromWorld = m_fromWorld;
+	}
+}
