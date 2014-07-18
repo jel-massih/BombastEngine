c37cd45ec5318503c84d8f81a4ccd64c94cf5e06
Jason El-Massih <jel-massih@hotmail.com>
2014-07-18 07:27:43 -0400

    Acreated SceneNode interface

--- Source/Graphics3D/SceneNode.h
+++ Source/Graphics3D/SceneNode.h
@@ -4,8 +4,10 @@
 #include "../Actor/Actor.h"
 #include "Geometry.h"
 #include "Material.h"
+#include <vector>
 
 class SceneNode;
+class BaseRenderComponent;
 
 //Defines available types of Alpha BLending
 enum AlphaType
@@ -48,6 +50,55 @@ protected:
 	AlphaType m_alphaType;
 
 	void SetAlpha(const float alpha) { m_alphaType = AlphaMaterial; m_material.SetAlpha(alpha); }
+};
+
+typedef std::vector<ISceneNode*> SceneNodeList;
+
+class SceneNode : public ISceneNode
+{
+	friend class Scene;
+
+public:
+	SceneNode(ActorId actorId, BaseRenderComponent* renderComponent, RenderPass renderPass, const Mat4x4* to, const Mat4x4* from=NULL);
+
+	virtual ~SceneNode();
+
+	virtual const SceneNodeProperties* const VGet() const { return &m_properties; }
+
+	virtual void VSetTransform(const Mat4x4* toWorld, const Mat4x4* fromWorld = NULL);
+	
+	virtual HRESULT VOnRestore(Scene* pScene);
+	virtual HRESULT VOnUpdate(Scene* pScene, DWORD const elapsedMs);
+
+	virtual HRESULT VPreRender(Scene* pScene);
+	virtual bool VIsVisible(Scene* pScene) const;
+	virtual HRESULT VRender(Scene* pScene) { return S_OK; }
+	virtual HRESULT VRenderChildren(Scene* pScene);
+	virtual HRESULT VPostRender(Scene* pScene);
+
+	virtual bool VAddChild(ISceneNode* kid);
+	virtual bool VRemoveChild(ActorId id);
+	virtual HRESULT VOnLostDevice(Scene* pScene);
+	virtual HRESULT VPick(Scene* pScene, RayCast* pRayCast);
+
+	void SetAlpha(float alpha);
+	float GetAlpha() const { return m_properties.GetAlpha(); }
+
+	Vec3 GetPosition() const { return m_properties.m_toWorld.GetPosition(); }
+	void SetPosition(const Vec3& pos) { m_properties.m_toWorld.SetPosition(pos); }
+
+	const Vec3 GetWorldPosition() const;
+
+	Vec3 GetDirection() const { return m_properties.m_toWorld.GetDirection(); }
+
+	void SetRadius(const float radius) { m_properties.m_radius = radius; }
+	void SetMaterial(const Material& mat) { m_properties.m_material = mat; }
+
+protected:
+	SceneNodeList m_children;
+	SceneNode* m_pParent;
+	SceneNodeProperties m_properties;
+	BaseRenderComponent* m_pRenderComponent;
 };
 
 #endif
