8a160ea086a3349dccf6d962e22df75a881654e9
Jason El-Massih <jel-massih@hotmail.com>
2014-07-18 08:39:22 -0400

    Declared RenderComponent.h File

--- Source/Actor/RenderComponent.h
+++ Source/Actor/RenderComponent.h
@@ -1,9 +1,29 @@
 #ifndef RENDER_COMPONENT_H
 #define RENDER_COMPONENT_H
 
-
+#include "RenderComponentInterface.h"
+#include "../Utilities/rapidxml.hpp"
 
 class BaseRenderComponent : public RenderComponentInterface
 {
+public:
+	virtual bool VInitialize(rapidxml::xml_node<>* pData) override;
+	virtual void VPostInit() override;
+	virtual void VOnChanged() override;
+	virtual rapidxml::xml_node<>* VGenerateXml() override;
+	const Color GetColor() const { return m_color; }
 
+protected:
+	virtual bool VDelegateInit(rapidxml::xml_node<>* pData) { return true; }
+	virtual SceneNode* VCreateSceneNode() = 0;
+	Color LoadColor(rapidxml::xml_node<>* pData);
+
+private:
+	virtual SceneNode* VGetSceneNode() override;
+
+protected:
+	Color m_color;
+	SceneNode* m_pSceneNode;
 };
+
+#endif
