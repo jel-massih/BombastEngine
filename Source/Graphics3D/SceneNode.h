a3db0767f2edc71fe3acfb56e4217ca80c844cda
Jason El-Massih <jel-massih@hotmail.com>
2014-07-17 20:03:03 -0400

    Completed rest of SceneNodeInterface  declarations

--- Source/Graphics3D/SceneNode.h
+++ Source/Graphics3D/SceneNode.h
@@ -21,6 +21,22 @@ class SceneNodeProperties
 {
 	friend class SceneNode;
 public:
+	SceneNodeProperties();
+	const ActorId& GetActorId() const { return m_actorId; }
+	Mat4x4 const& ToWorld() const { return m_toWorld; }
+	Mat4x4 const& FromWorld() const { return m_fromWorld; }
+	void Transform(Mat4x4* toWorld, Mat4x4* fromWorld) const;
+
+	const char* GetName() const { return m_name.c_str(); }
+
+	bool HasAlpha() const { return m_name.c_str(); }
+	float GetAlpha() const { return m_material.GetAlpha(); }
+	AlphaType GetAlphaType() const { return m_alphaType; }
+
+	RenderPass GetRenderPass() const { return m_renderPass; }
+	float GetRadius() const { return m_radius; }
+
+	Material GetMaterial() const { return m_material; }
 
 protected:
 	ActorId m_actorId;
@@ -31,6 +47,7 @@ protected:
 	Material m_material;
 	AlphaType m_alphaType;
 
+	void SetAlpha(const float alpha) { m_alphaType = AlphaMaterial; m_material.SetAlpha(alpha); }
 };
 
 #endif
