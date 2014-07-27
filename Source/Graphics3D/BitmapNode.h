c9bc9efb2437b4609041aafd17fad36292eac322
Jason El-Massih <jel-massih@hotmail.com>
2014-07-26 21:01:57 -0400

    Defined D3D11 Bitmap Version

--- Source/Graphics3D/BitmapNode.h
+++ Source/Graphics3D/BitmapNode.h
@@ -17,4 +17,17 @@ protected:
 	std::string GetTextureName(const int side);
 };
 
+class D3DBitmapNode11 : public BitmapNode
+{
+public:
+	D3DBitmapNode11(const char* pTextureBaseNode);
+	virtual ~D3DBitmapNode11();
+	HRESULT VOnRestore(Scene* pScene);
+	HRESULT VRender(Scene* pScene);
+
+protected:
+	ID3D11Buffer* m_pIndexBuffer;
+	ID3D11Buffer* m_pVertexBuffer;
+};
+
 #endif
