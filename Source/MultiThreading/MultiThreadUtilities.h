7f5e55d4534bad6085d0e44a6bee00f4aebd9cf3
Jason El-Massih <jel-massih@hotmail.com>
2014-10-08 21:28:14 -0400

    Add Multithreading Utilities

---
+++ Source/MultiThreading/MultiThreadUtilities.h
@@ -0,0 +1,54 @@
+#ifndef MULTI_THREAD_UTILITIES
+#define MULTI_THREAD_UTILITIES
+
+#include <mutex>
+#include <queue>
+
+template <typename T>
+class concurrent_queue
+{
+public:
+	void push(T const& data)
+	{
+		std::unique_lock<std::mutex> mlock(m_mutex);
+		m_queue.push(data);
+		mlock.unlock();
+		m_cond.notify_one();
+	}
+
+	bool empty() const
+	{
+		std::unique_lock<std::mutex> mlock(m_mutex);
+		return m_queue.empty();
+	}
+
+	bool try_pop(T& popped_val)
+	{
+		std::unique_lock<std::mutex> mlock(m_mutex);
+		if(m_queue.empty) {
+			return false;
+		}
+
+		popped_val = m_queue.front();
+		m_queue.pop();
+		return true;
+	}
+
+	void wait_and_pop(T& popped_val)
+	{
+		std::unique_lock<std::mutex> mlock(m_mutex);
+		while(m_queue.empty())
+		{
+			m_cond.wait(mlock);
+		}
+
+		popped_val = m_queue.front();
+		m_queue.pop();
+	}
+private:
+	std::mutex m_mutex;
+	std::queue<T> m_queue;
+	std::condition_variable m_cond;
+};
+
+#endif
