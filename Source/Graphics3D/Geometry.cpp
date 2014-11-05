b390f0e3c8275e7a0ff801654ab2132c87b11286
Jason El-Massih <jel-massih@hotmail.com>
2014-11-04 22:31:53 -0500

    New Vector Classes and methods Defined

--- Source/Graphics3D/Geometry.cpp
+++ Source/Graphics3D/Geometry.cpp
@@ -2,3 +2,128 @@
 #include "Geometry.h"
 
 const Mat4x4 Mat4x4::g_Identity(XMFLOAT4X4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
+
+Vec3 Vec3::operator*(float f) const
+{
+	Vec3 v = Vec3(x*f, y*f, z*f);
+	return v;
+}
+
+Vec3 Vec3::operator-(Vec3 v) const
+{
+	Vec3 v2 = Vec3(x - v.x, y - v.y, z - v.z);
+
+	return v2;
+}
+
+Vec3 Vec3::operator+(Vec3 v) const
+{
+	Vec3 v2 = Vec3(x + v.x, y + v.y, z + v.z);
+
+	return v2;
+}
+
+Vec3& Vec3::operator+=(const Vec3 v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+
+	return *this;
+}
+
+Vec3 operator* (float f, const Vec3& vec)
+{
+	return vec * f;
+}
+
+bool Plane::Inside(const Vec3& point) const
+{
+	XMFLOAT4 planeDotProduct;
+	
+	XMStoreFloat4(&planeDotProduct, XMPlaneDotCoord(XMLoadFloat4(this), point.Vect()));
+	
+	return (planeDotProduct.x >= 0.0f);
+}
+
+bool Plane::Inside(const Vec3& point, const float radius) const
+{
+	XMFLOAT4 planeDotProduct;
+	XMStoreFloat4(&planeDotProduct, XMPlaneDotCoord(XMLoadFloat4(this), point.Vect()));
+
+	return (planeDotProduct.x >= -radius);
+}
+
+Frustum::Frustum()
+{
+	m_fov = BE_PI / 4.0f; //Default to 90
+	m_aspect = 1.0f;
+	m_near = 1.0f;
+	m_far = 1000.0f;
+}
+
+bool Frustum::Inside(const Vec3& point) const
+{
+	for (int i = 0; i < NumPlanes; i++)
+	{
+		if (!m_planes[i].Inside(point))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool Frustum::Inside(const Vec3& point, const float radius) const
+{
+	for (int i = 0; i < NumPlanes; i++)
+	{
+		if (!m_planes[i].Inside(point, radius))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void Frustum::Init(const float fov, const float aspect, const float nearClip, const float farClip)
+{
+	m_fov = fov;
+	m_aspect = aspect;
+	m_near = nearClip;
+	m_far = farClip;
+	
+	float tanFovOver2 = (float)tan(m_fov / 2.0f);
+	Vec3 nearRight = (m_near * tanFovOver2) * m_aspect * g_Right;
+	Vec3 farRight = (m_far * tanFovOver2) * m_aspect * g_Right;
+	Vec3 nearUp = (m_near * tanFovOver2) * g_Up;
+	Vec3 farUp = (m_far * tanFovOver2) * g_Up;
+
+	m_nearClip[0] = (m_near * g_Forward) - nearRight + nearUp;
+	m_nearClip[1] = (m_near * g_Forward) + nearRight + nearUp;
+	m_nearClip[2] = (m_near * g_Forward) + nearRight - nearUp;
+	m_nearClip[3] = (m_near * g_Forward) - nearRight - nearUp;
+
+	m_farClip[0] = (m_far * g_Forward) - farRight + farUp;
+	m_farClip[1] = (m_far * g_Forward) + farRight + farUp;
+	m_farClip[2] = (m_far * g_Forward) + farRight - farUp;
+	m_farClip[3] = (m_far * g_Forward) - farRight - farUp;
+
+	//Construct 6 planes from the 8 points
+	//Normals point awar if use CCW verts
+
+	Vec3 origin(0.0f, 0.0f, 0.0f);
+	m_planes[Near].Init(m_nearClip[2], m_nearClip[1], m_nearClip[0]);
+	m_planes[Far].Init(m_farClip[0], m_farClip[1], m_farClip[2]);
+	m_planes[Right].Init(m_farClip[2], m_farClip[1], origin);
+	m_planes[Top].Init(m_farClip[1], m_farClip[0], origin);
+	m_planes[Left].Init(m_farClip[0], m_farClip[3], origin);
+	m_planes[Bottom].Init(m_farClip[3], m_farClip[2], origin);
+}
+
+void Frustum::Render()
+{
+	//@TODO: not Implemented Yet
+}
--- Source/Graphics3D/Geometry.h
+++ Source/Graphics3D/Geometry.h
@@ -6,6 +6,32 @@ using namespace DirectX;
 const float  BE_PI = 3.14159265358979f;
 const float  BE_2PI = 2 * BE_PI;
 
+class Vec4 : public XMFLOAT4
+{
+public:
+	inline float Length() { XMVectorGetX(XMVector4Length(XMLoadFloat4(this))); }
+	inline Vec4* Normalize()
+	{
+		Vec4* v;
+		XMStoreFloat4(v, XMVector4Normalize(XMLoadFloat4(this)));
+
+		return v;
+	}
+	inline float Dot(const Vec4 &b)
+	{
+		return XMVectorGetX(XMVector4Dot(XMLoadFloat4(this), XMLoadFloat4(&b)));
+	}
+	inline float Dot(const XMVECTOR &v)
+	{
+		return XMVectorGetX(XMVector4Dot(XMLoadFloat4(this), v));
+	}
+
+	Vec4(XMFLOAT4& v4) { x = v4.x; y = v4.y; z = v4.z; w = v4.w; }
+	Vec4() : XMFLOAT4() {}
+	Vec4(const float _x, const float _y, const float _z, const float _w) { x = _x; y = _y; z = _z; w = _w; }
+	//Vec4(const Vec3& v3) { x = v3.x; y = v3.y; z = v3.z; w = 1.0f; }
+};
+
 class Vec3 : public XMFLOAT3
 {
 public:
@@ -13,12 +39,21 @@ public:
 	inline Vec3 Normalize() { Vec3 dest;  XMStoreFloat3(&dest, XMVector3Normalize(XMLoadFloat3(this))); return dest; }
 	inline float Dot(const Vec3 &b) { float f = 0.0; XMStoreFloat(&f, XMVector3Dot(XMLoadFloat3(this), XMLoadFloat3(&b))); return f; }
 	inline Vec3 Cross(const Vec3 &b) const { Vec3 out; XMStoreFloat3(&out, XMVector3Cross(XMLoadFloat3(this), XMLoadFloat3(&b))); return out;}
+	inline const XMVECTOR Vect() const { return XMLoadFloat3(this); }
 
 	Vec3(XMFLOAT3 &v3) { x = v3.x; y = v3.y; z = v3.z; }
 	Vec3() : XMFLOAT3() { x = 0; y = 0; z = 0; }
 	Vec3(const float _x, const float _y, const float _z) { x = _x; y = _y; z = _z; }
 	Vec3(const double _x, const double _y, const double _z) { x = (float)_x; y = (float)_y; z = (float)_z; }
+	inline Vec3(const class Vec4& v4);
+
+	Vec3 operator*(float f) const;
+	Vec3 operator-(Vec3 v) const;
+	Vec3 operator+(Vec3 v) const;
+	Vec3& operator+=(const Vec3 v);
 };
+
+inline Vec3::Vec3(const Vec4& v4) { x = v4.x; y = v4.y; z = v4.z; }
 
 extern Vec3 g_Up;
 extern Vec3 g_Right;
@@ -37,6 +72,7 @@ public:
 	inline Vec3 GetYawPitchRoll() const;
 	inline Vec3 GetScale() const;
 	inline Vec3 Xform(Vec3 &v) const;
+	inline Vec4 Xform(Vec4 &v) const;
 	inline Mat4x4 Inverse() const;
 
 	Mat4x4(XMFLOAT4X4& mat) { memcpy(&m, &mat.m, sizeof(mat.m)); }
@@ -136,6 +172,13 @@ inline Vec3 Mat4x4::Xform(Vec3& v) const
 	return Vec3(out.x, out.y, out.z);
 }
 
+inline Vec4 Mat4x4::Xform(Vec4& v) const
+{
+	Vec4 temp;
+	XMStoreFloat4(&temp, XMVector4Transform(XMLoadFloat4(&v), XMLoadFloat4x4(this)));
+	return temp;
+}
+
 inline Mat4x4 Mat4x4::Inverse() const
 {
 	Mat4x4 out;
@@ -173,5 +216,62 @@ inline Mat4x4 operator * (const Mat4x4& a, const Mat4x4& b)
 	XMStoreFloat4x4(&out, XMMatrixMultiply(XMLoadFloat4x4(&a), XMLoadFloat4x4(&b)));
 	return out;
 }
+
+//Plane
+class Plane : public XMFLOAT4
+{
+public:
+	inline void Normalize();
+
+	inline void Init(const Vec3& p0, const Vec3& p1, const Vec3& p2);
+	bool Inside(const Vec3& point, const float radius) const;
+	bool Inside(const Vec3& point) const;
+};
+
+inline void Plane::Normalize()
+{
+	float length;
+	length = sqrt(x*x + y*y + z*z);
+	x = x / length;
+	y = y / length;
+	z = z / length;
+	w = w / length;
+}
+
+inline void Plane::Init(const Vec3& p0, const Vec3& p1, const Vec3& p2)
+{
+	XMStoreFloat4(this, XMPlaneFromPoints(p0.Vect(), p1.Vect(), p2.Vect()));
+	Normalize();
+}
+
+//Frustum
+class Frustum
+{
+public:
+	enum Side{Near, Far, Top, Right, Bottom, Left, NumPlanes};
+
+	Plane m_planes[NumPlanes];	//Planes of Frustum in Camera Space
+	Vec3 m_nearClip[4];		    //Verts of near clip plane in cam space
+	Vec3 m_farClip[4];			//verts of far clip plane
+
+	float m_fov;
+	float m_aspect;
+	float m_near;
+	float m_far;
+
+public:
+	Frustum();
+
+	bool Inside(const Vec3& point) const;
+	bool Inside(const Vec3& point, const float radius) const;
+	const Plane& Get(Side side) { return m_planes[side]; }
+	void SetFov(float fov) { m_fov = fov; Init(m_fov, m_aspect, m_near, m_far); }
+	void SetAspect(float aspect) { m_aspect = aspect; Init(m_fov, m_aspect, m_near, m_far); }
+	void SetNear(float nearClip) { m_near = nearClip; Init(m_fov, m_aspect, m_near, m_far); }
+	void SetFar(float farClip) { m_far = farClip; Init(m_fov, m_aspect, m_near, m_far); }
+	void Init(const float fov, const float aspect, const float near, const float far);
+
+	void Render();
+};
 
 #endif
