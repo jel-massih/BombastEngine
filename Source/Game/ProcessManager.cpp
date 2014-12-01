42f4730c7356f11c9117916973a3847e80f6106b
Jason El-Massih <jel-massih@hotmail.com>
2014-11-30 23:26:55 -0500

    Added ProcessManager to project

---
+++ Source/Game/ProcessManager.cpp
@@ -0,0 +1,104 @@
+#include "ProcessManager.h"
+
+ProcessManager::~ProcessManager()
+{
+	ClearAllProcesses();
+}
+
+unsigned int ProcessManager::UpdateProcesses(unsigned long deltaMs)
+{
+	unsigned short int successCount = 0;
+	unsigned short int failCount = 0;
+
+	ProcessList::iterator it = m_processList.begin();
+	while (it != m_processList.end())
+	{
+		Process* pCurrProcess = (*it);
+
+		ProcessList::iterator thisIt = it;
+		it++;
+
+		if (pCurrProcess->GetState() == Process::UNINITIALIZED)
+		{
+			pCurrProcess->VOnInit();
+		}
+
+		if (pCurrProcess->GetState() == Process::RUNNING)
+		{
+			pCurrProcess->VOnUpdate(deltaMs);
+		}
+
+		if (pCurrProcess->IsDead())
+		{
+			switch (pCurrProcess->GetState())
+			{
+				case Process::SUCCEEDED:
+				{
+					pCurrProcess->VOnSuccess();
+					Process* pChild = pCurrProcess->RemoveChild();
+					if (pChild)
+					{
+						AttachProcess(pChild);
+					}
+					else
+					{
+						successCount++;
+					}
+
+					break;
+				}
+
+				case Process::FAILED:
+				{
+					pCurrProcess->VOnFail();
+					failCount++;
+					break;
+				}
+
+				case Process::ABORTED:
+				{
+					pCurrProcess->VOnAbort();
+					failCount++;
+					break;
+				}
+			}
+
+			m_processList.erase(thisIt);
+		}
+	}
+
+	return ((successCount << 16) | failCount);
+}
+
+Process* ProcessManager::AttachProcess(Process* pProcess)
+{
+	m_processList.push_front(pProcess);
+	return pProcess;
+}
+
+void ProcessManager::ClearAllProcesses()
+{
+	m_processList.clear();
+}
+
+void ProcessManager::AbortAllProcesses(bool immediate)
+{
+	ProcessList::iterator it = m_processList.begin();
+
+	while (it != m_processList.end())
+	{
+		ProcessList::iterator tempIt = it;
+		it++;
+
+		Process* pProcess = *tempIt;
+		if (pProcess->IsAlive())
+		{
+			pProcess->SetState(Process::ABORTED);
+			if (immediate)
+			{
+				pProcess->VOnAbort();
+				m_processList.erase(tempIt);
+			}
+		}
+	}
+}
