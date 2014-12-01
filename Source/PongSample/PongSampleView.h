27020115547acab75f194ed3faf2f8cd1b6742b4
Jason El-Massih <jel-massih@hotmail.com>
2014-12-01 00:35:40 -0500

    Added Initial PongSampleView Class

---
+++ Source/PongSample/PongSampleView.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../Game/HumanView.h"
+#include "PongSampleController.h"
+
+class PongSampleHumanView : public HumanView
+{
+public:
+	PongSampleHumanView(IRenderer* renderer);
+	virtual ~PongSampleHumanView();
+
+	virtual LRESULT CALLBACK VOnMsgProc(AppMsg msg);
+	virtual void VOnUpdate(unsigned long deltaMs);
+	virtual void VOnAttach(GameViewId vid, ActorId aid);
+
+	virtual void VSetControlledActor(ActorId actorId);
+	virtual bool VLoadGameDelegate(rapidxml::xml_node<>* pLevelData) override;
+
+	void SetControlledActorDelegate(IEventDataPtr pEventData);
+
+protected:
+	bool m_bShowUI;
+	std::wstring m_gameplayText;
+
+	PongSampleController* m_pPongController;
+	SceneNode* m_pTeapot;
+
+private:
+	void RegisterAllDelegates();
+	void RemoveAllDelegates();
+};
