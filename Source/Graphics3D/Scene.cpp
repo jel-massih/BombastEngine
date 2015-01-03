2e6ce5e72b312e6592691348501b560acdb2e740
Jason El-Massih <jel-massih@hotmail.com>
2015-01-03 15:44:25 -0500

    Set view transform when camera initially created

--- Source/Graphics3D/Scene.cpp
+++ Source/Graphics3D/Scene.cpp
@@ -19,6 +19,7 @@ Scene::Scene(IRenderer* renderer)
 	frustum.Init(BE_PI / 4.0f, 1.0f, 1.0f, 100.0f);
 	m_pCamera = BE_NEW CameraNode(&Mat4x4::g_Identity, frustum);
 	AddChild(INVALID_ACTOR_ID, m_pCamera);
+	m_pCamera->SetViewTransform(this);
 
 	m_pGrid = BE_NEW D3D11GridNode(INVALID_ACTOR_ID, nullptr, &Mat4x4::g_Identity);
 	AddChild(INVALID_ACTOR_ID, m_pGrid);
