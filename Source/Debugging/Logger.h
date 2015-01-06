18987d7200107b29fa4632aa362683a08ad58d75
Jason El-Massih <jel-massih@hotmail.com>
2015-01-05 20:56:51 -0500

    Add some useful Logging macros

--- Source/Debugging/Logger.h
+++ Source/Debugging/Logger.h
@@ -13,32 +13,47 @@ namespace BELogger
 
 #define BE_ERROR(str) \
 	do { \
+		BELogger::Log("ERROR", str, __FUNCTION__, __FILE__, __LINE__); \
 		std::wstring s(s2ws(str)); \
 		if(g_pApp->GetHwnd() != NULL) { \
 			MessageBox(g_pApp->GetHwnd(), s.c_str(), L"Error", MB_OK); \
-		} else { \
+				} else { \
 			OutputDebugString(s.c_str()); \
-		} \
-		} while(0) \
+				} \
+			} while(0) \
 
 #define BE_ASSERT(expr) \
-do \
-{ \
-	if (!(expr)) \
-	{ \
-		assert(expr); \
-	} \
-} \
-while (0) \
+	do { \
+		if (!(expr)) \
+				{ \
+			assert(expr); \
+				} \
+		} while (0) \
 
 #define BE_ASSERT_MESSAGE(expr, message) \
-do \
-{ \
-	if (!(expr)) \
-		{ \
-		assert(expr && message); \
-		} \
-} \
-while (0) \
+	do { \
+		if (!(expr)) \
+					{ \
+			assert(expr && message); \
+					} \
+		} while (0) \
+
+#define BE_WARNING(str) \
+	do { \
+		std::string s(str); \
+		BELogger::Log("WARNING", s, __FUNCTION__, __FILE__, __LINE__); \
+		} while(0) \
+
+#define BE_INFO(str) \
+	do { \
+		std::string s(str); \
+		BELogger::Log("INFO", s, NULL, NULL, 0); \
+			} while(0) \
+
+#define BE_LOG(tag, str) \
+	do { \
+		std::string s(str); \
+		BELogger::Log(tag, s, NULL, NULL, 0); \
+			} while(0) \
 
 #define BE_HRETURN(x, str)    { hr = (x); if( FAILED(hr) ) { BE_ERROR( str ); return hr; } }
