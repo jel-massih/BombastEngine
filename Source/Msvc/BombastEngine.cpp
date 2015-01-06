b7b574baa09f162c6a3eaed5a597d7e0ea493d7e
Jason El-Massih <jel-massih@hotmail.com>
2015-01-05 19:35:12 -0500

    Create Debug Console Window if option enabled

--- Source/Msvc/BombastEngine.cpp
+++ Source/Msvc/BombastEngine.cpp
@@ -10,6 +10,8 @@ int WINAPI BombastEngine(HINSTANCE hInstance,
                    LPWSTR lpCmdLine,
                    int nCmdShow)
 {
+	bool bConsoleEnabled = false;
+
 	//Setup Checks for memory leaks
 	int tmpDbgFlag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
 
@@ -20,6 +22,17 @@ int WINAPI BombastEngine(HINSTANCE hInstance,
 
 	g_pApp->m_options.Init((ROOT_GAME_PATH + "Options.xml").c_str(), lpCmdLine);
 
+	if (g_pApp->m_options.m_bDebugConsoleEnabled)
+	{
+		AllocConsole();
+
+		FILE* pCout;
+		freopen_s(&pCout, "CONIN$", "r", stdin);
+		freopen_s(&pCout, "CONOUT$", "w", stdout);
+		freopen_s(&pCout, "CONOUT$", "w", stderr);
+	}
+
+
 	if (g_pApp->InitInstance(hInstance, lpCmdLine, 0, g_pApp->m_options.m_screenSize.x, g_pApp->m_options.m_screenSize.y, g_pApp->m_options.m_screenPosition.x, g_pApp->m_options.m_screenPosition.y))
 	{
 		g_pApp->Run();
@@ -27,5 +40,10 @@ int WINAPI BombastEngine(HINSTANCE hInstance,
 
 	g_pApp->ShutDown();
 	
+	if (bConsoleEnabled)
+	{
+		FreeConsole();
+	}
+
 	return 0;
 }
