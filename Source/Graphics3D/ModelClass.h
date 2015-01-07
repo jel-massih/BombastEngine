0ede684852b84994360f4ae316b7c40df12e952f
Jason El-Massih <jel-massih@hotmail.com>
2015-01-07 00:29:04 -0500

    Updated ModelClass to be a more core data representation

--- Source/Graphics3D/ModelClass.h
+++ Source/Graphics3D/ModelClass.h
@@ -4,36 +4,30 @@
 
 class ModelClass
 {
-private:
-	struct VertexType
+public:
+	struct FaceType
 	{
-		DirectX::XMFLOAT3 position;
-		DirectX::XMFLOAT2 texture;
+		int vIndex1, vIndex2, vIndex3;
+		int tIndex1, tIndex2, tIndex3;
+		int nIndex1, nIndex2, nIndex3;
 	};
 
 public:
 	ModelClass();
-	ModelClass(const ModelClass& other);
-	~ModelClass();
 
-	bool Initialize(ID3D11Device* device, std::string textureFilepath);
-	void Shutdown();
-	void Render(ID3D11DeviceContext* context);
+	void SetVertexData(Vec3* vertexData, int count) { m_vertexData = vertexData; m_vertexCount = count; }
+	void SetTextureData(Vec3* textureData, int count) { m_textureData = textureData; m_textureCount = count; }
+	void SetNormalData(Vec3* normalData, int count) { m_normalData = normalData; m_normalCount = count; }
+	void SetFaceData(FaceType* faceData, int count) { m_faceData = faceData; m_faceCount = count; }
 
-	int GetIndexCount();
-
-	ID3D11ShaderResourceView* GetTexture();
-
-private:
-	bool InitializeBuffers(ID3D11Device* device);
-	void ShutdownBuffers();
-	void RenderBuffers(ID3D11DeviceContext* context);
-
-	bool LoadTexture(ID3D11Device* device, std::string textureFilepath);
-	void ReleaseTexture();
+	Vec3* GetVertexData() { return m_vertexData; }
+	Vec3* GetTextureData() { return m_textureData; }
+	Vec3* GetNormalData() { return m_normalData; }
+	FaceType* GetFaceData() { return m_faceData; }
 
 private:
-	ID3D11Buffer* m_pVertexBuffer, *m_pIndexBuffer;
-	int m_vertexCount, m_indexCount;
-	TextureClass* m_pTexture;
+	Vec3* m_vertexData, *m_textureData, *m_normalData;
+	FaceType* m_faceData;
+
+	int m_vertexCount, m_textureCount, m_normalCount, m_faceCount;
 };
