cbc5c25ac3995a327b82021f124e658ebfde906d
Jason El-Massih <jel-massih@hotmail.com>
2015-01-10 19:06:33 -0500

    Remove tinyobj from project

--- Engine/Source/Resources/ModelResource.cpp
+++ Engine/Source/Resources/ModelResource.cpp
@@ -2,43 +2,6 @@
 #include "../Bombast/BombastApp.h"
 #include <sstream>
 
-class MaterialResourceReader :
-	public tinyobj::MaterialReader
-{
-public:
-	MaterialResourceReader(const std::string& mtl_basepath) : m_mtlBasePath(mtl_basepath) {}
-	virtual ~MaterialResourceReader() {}
-	virtual std::string operator() (
-		const std::string& matId,
-		std::vector<tinyobj::material_t>& materials,
-		std::map<std::string, int>& matMap);
-
-private:
-	std::string m_mtlBasePath;
-};
-
-std::string MaterialResourceReader::operator() (
-	const std::string& matId,
-	std::vector<tinyobj::material_t>& materials,
-	std::map<std::string, int>& matMap)
-{
-	std::string filepath;
-
-	if (!m_mtlBasePath.empty()) {
-		filepath = std::string(m_mtlBasePath) + matId;
-	}
-	else {
-		filepath = matId;
-	}
-
-	Resource resource(filepath);
-	ResourceHandle* pResourceHandle = g_pApp->m_pResourceCache->GetHandle(&resource);
-
-	std::stringstream ss(pResourceHandle->Buffer());
-
-	return tinyobj::LoadMtl(matMap, materials, ss);
-}
-
 void ModelResourceExtraData::Shutdown()
 {
 	SAFE_DELETE(m_pModelData);
@@ -60,14 +23,10 @@ bool ModelResourceExtraData::LoadModel(char* pRawBuffer, unsigned int rawSize, s
 	m_pModelData = BE_NEW ModelClass;
 	std::istringstream ss(pRawBuffer);
 	unsigned int slashIndex = modelFilepath.find_last_of("\\");
-	MaterialResourceReader matResourceReader(modelFilepath.substr(0, slashIndex+1));
-	std::string result = tinyobj::LoadObj(m_pModelData->shapes, m_pModelData->materials, ss, matResourceReader);
-	if (!result.empty()) {
-		BE_ERROR(result);
-		return false;
-	}
 
-	return true;
+
+
+	return false;
 }
 
 bool ModelResourceLoader::VLoadResource(char* rawBuffer, unsigned int rawSize, ResourceHandle* handle)
