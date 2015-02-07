493906ac800ffb76da9bf2fa42ae66053e5e8fcb
Jason El-Massih <jel-massih@hotmail.com>
2015-02-06 21:11:23 -0500

    Fix up logging Macros to work with new system

--- Engine/Source/Debugging/Logger.h
+++ Engine/Source/Debugging/Logger.h
@@ -11,16 +11,7 @@ namespace BELogger
 	void SetDisplayFlags(const char* tag, unsigned char flags);
 }
 
-#define BE_ERROR(str) \
-	do { \
-		BELogger::Log("ERROR", str, __FUNCTION__, __FILE__, __LINE__); \
-		std::wstring s(s2ws(str)); \
-		if(g_pApp->GetHwnd() != NULL) { \
-			MessageBox(g_pApp->GetHwnd(), s.c_str(), L"Error", MB_OK); \
-				} else { \
-			OutputDebugString(s.c_str()); \
-				} \
-			} while(0) \
+#define BE_ERROR(str, ...) BELogger::Log("ERROR", str, __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define BE_ASSERT(expr) \
 	do { \
@@ -38,24 +29,12 @@ namespace BELogger
 					} \
 		} while (0) \
 
-#define BE_WARNING(str) \
-	do { \
-		std::string s(str); \
-		BELogger::Log("WARNING", s, __FUNCTION__, __FILE__, __LINE__); \
-		} while(0) \
+#define BE_WARNING(str, ...) BELogger::Log("WARNING", s, __FUNCTION__, __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define BE_INFO(str) \
-	do { \
-		std::string s(str); \
-		BELogger::Log("INFO", s, NULL, NULL, 0); \
-			} while(0) \
+#define BE_INFO(str, ...) BE_LOG("INFO", s, ##__VA_ARGS__)
 
-#define BE_LOG_GRAPHICS(str) BE_LOG("graphics", str);
+#define BE_LOG_GRAPHICS(str, ...) BE_LOG("graphics", str, ##__VA_ARGS__)
 
-#define BE_LOG(tag, str, ...) \
-	do { \
-		std::string s(str); \
-		BELogger::Log(tag, s, NULL, NULL, 0); \
-			} while(0) \
+#define BE_LOG(tag, str, ...) BELogger::Log(tag, s, NULL, NULL, 0, ##__VA_ARGS__)
 
-#define BE_HRETURN(x, str)    { hr = (x); if( FAILED(hr) ) { BE_ERROR( str ); return hr; } }
+#define BE_HRETURN(x, str) { hr = (x); if( FAILED(hr) ) { BE_ERROR( str ); return hr; } }
