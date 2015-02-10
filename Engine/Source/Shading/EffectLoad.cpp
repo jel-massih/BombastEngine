6bc42f01829a073f54ec0288f279653958c126aa
Jason El-Massih <jel-massih@hotmail.com>
2015-02-09 22:36:47 -0500

    Fix Compile Error

--- Engine/Source/Shading/EffectLoad.cpp
+++ Engine/Source/Shading/EffectLoad.cpp
@@ -4,5 +4,7 @@ Effect* Effect::Create(const char* name)
 {
 	//@TODO: Check if effects full
 
-	BE_GRAPHICS("Loading Effect: " + std::string(name));
+	//BE_LOG_GRAPHICS("Loading Effect: %s", name);
+
+	return NULL;
 }
