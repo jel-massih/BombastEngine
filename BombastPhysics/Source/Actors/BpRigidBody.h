0901526d257756d07b302a7a854d47a605829334
Jason El-Massih <jel-massih@hotmail.com>
2015-03-18 14:54:32 -0400

    Add BpRigidBody class

---
+++ BombastPhysics/Source/Actors/BpRigidBody.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "BpRigidActor.h"
+
+namespace bPhysics
+{
+
+	class BpRigidBody : public BpRigidActor
+	{
+	public:
+
+	protected:
+		inline BpRigidBody() {}
+		virtual ~BpRigidBody() {}
+	};
+
+}
