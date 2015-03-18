d442bee462604718b252aa8ddf40e43062e99efd
Jason El-Massih <jel-massih@hotmail.com>
2015-03-18 14:54:12 -0400

    Add BpRigidActor class

---
+++ BombastPhysics/Source/Actors/BpRigidActor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "BpActor.h"
+
+namespace bPhysics
+{
+	class BpRigidActor : public BpActor
+	{
+	public:
+		//Retrieves actors world space transform
+		virtual BpMat4x4 GetWorldTransform() const = 0;
+		virtual void SetWorldTransform(const BpMat4x4& newTransform, bool autoWake = true) = 0;
+	
+	protected:
+		inline BpRigidActor() {}
+		virtual ~BpRigidActor() {}
+	};
+}
