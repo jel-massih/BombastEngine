4c8e2430e0113b8c04650c02c5e2ca3ab4b868d2
Jason El-Massih <jel-massih@hotmail.com>
2015-03-18 14:56:06 -0400

    Added GetWorldBOunds to actor class

--- BombastPhysics/Source/Actors/BpActor.h
+++ BombastPhysics/Source/Actors/BpActor.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../Geometry/BpBounds3.h"
+
 namespace bPhysics
 {
 	class BpScene;
@@ -22,7 +24,7 @@ namespace bPhysics
 		virtual void SetName(const char* name) = 0;
 		virtual const char* GetName() const = 0;
 
-
+		virtual BpBounds3 GetWorldBounds(float inflation = 1.01f) const = 0;
 
 	protected:
 		BpActor() {}
