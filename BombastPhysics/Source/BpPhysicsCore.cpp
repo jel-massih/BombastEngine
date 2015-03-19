3fa8dd1bcfd391b30d7ec3fd26e4536baddeb553
Jason El-Massih <jel-massih@hotmail.com>
2015-03-19 15:43:11 -0400

    Add CreateShape Method

--- BombastPhysics/Source/BpPhysicsCore.cpp
+++ BombastPhysics/Source/BpPhysicsCore.cpp
@@ -1,6 +1,9 @@
 #include "BpPhysicsCore.h"
 #include "BpMaterial.h"
 #include "Scene\BpScene.h"
+#include "Foundation\BpReporting.h"
+#include "Geometry\BpGeometrySphere.h"
+#include "Geometry\BpShape.h"
 
 using namespace bPhysics;
 
@@ -33,4 +36,48 @@ BpMaterial* BpPhysicsCore::CreateMaterial(float dynamicFriction, float staticFri
 	material->SetRestitution(restitution);
 
 	return material;
+}
+
+BpShape* BpPhysicsCore::CreateShape(const BpGeometry& geometry, BpMaterial* const * materials, unsigned short materialCount, bool isExclusive)
+{
+	if (!materials)
+	{
+		BP_ERROR("CreateShape: Material Pointer is NULL");
+		return nullptr;
+	}
+
+	if (materialCount == 0)
+	{
+		BP_ERROR("CreateShape: Material Count is 0");
+		return nullptr;
+	}
+
+	switch (geometry.GetType())
+	{
+	case BpGeometryType::SPHERE:
+		if (!static_cast<const BpGeometrySphere&>(geometry).IsValid())
+		{
+			BP_ERROR("CreateShape: Supplied BpGeometry is not valid");
+			return nullptr;
+		}
+		break;
+	default:
+		BP_ERROR("CreateShape: Invalid Geometry Type");
+		return nullptr;
+	}
+
+	if (materialCount > 1)
+	{
+		BP_ERROR("CreateShape: Currently Only 1 Material is supported");
+		return nullptr;
+	}
+
+	BpShape* bpShape = new BpShape(geometry, isExclusive);
+
+	if (!bpShape)
+	{
+		return nullptr;
+	}
+
+	return bpShape;
 }
--- BombastPhysics/Source/BpPhysicsCore.h
+++ BombastPhysics/Source/BpPhysicsCore.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "Geometry\BpGeometry.h"
+#include "BpMaterial.h"
+
 namespace bPhysics
 {
 	class BpScene;
 	class BpSceneDesc;
 	class BpMaterial;
+	class BpShape;
 
 	class BpPhysicsCore
 	{
@@ -15,6 +19,14 @@ namespace bPhysics
 		BpScene* CreateScene(const BpSceneDesc& sceneDesc);
 
 		BpMaterial* CreateMaterial(float dynamicFriction, float staticFriction, float restitution);
+
+		inline BpShape* CreateShape(const BpGeometry& geometry, const BpMaterial& material, bool isExclusive = false)
+		{
+			BpMaterial* mat = const_cast<BpMaterial*>(&material);
+			return CreateShape(geometry, &mat, 1, isExclusive);
+		}
+
+		virtual BpShape* CreateShape(const BpGeometry& geometry, BpMaterial* const * materials, unsigned short materialCount, bool isExclusive = false);
 
 	private:
 		BpScene* m_pScene;
