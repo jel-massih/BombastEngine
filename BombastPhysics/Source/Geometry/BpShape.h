439ae059c470633d0e18a7b6a8054777187a0dce
Jason El-Massih <jel-massih@hotmail.com>
2015-03-19 20:35:40 -0400

    Add Transform to shape class

--- BombastPhysics/Source/Geometry/BpShape.h
+++ BombastPhysics/Source/Geometry/BpShape.h
@@ -12,7 +12,10 @@ namespace bPhysics
 		BpShape(const BpGeometry& geometry,
 			bool isExclusive);
 
+		inline const BpMat4x4& GetWorldTransform() const { return m_transform; }
+		inline void SetWorldTransform(const BpMat4x4& t) { m_transform = t; }
+
 	private:
-		
+		BpMat4x4 m_transform;
 	};
 }
