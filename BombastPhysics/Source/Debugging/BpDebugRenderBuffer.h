696af18de28b633a77aaf1f412a7fcc6041429b9
Jason El-Massih <jel-massih@hotmail.com>
2015-04-02 19:23:50 -0400

    Add INitial Buffer file

---
+++ BombastPhysics/Source/Debugging/BpDebugRenderBuffer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../Foundation/BpMath.h"
+#include <vector>
+
+namespace bPhysics
+{
+	class BpDebugLine
+	{
+	public:
+		BpVec3 point1;
+		BpVec3 point2;
+		BpVec3 color;
+	};
+
+	class BpDebugSphere
+	{
+	public:
+		BpVec3 center;
+		float radius;
+		BpVec3 color;
+	};
+
+	class BpDebugRenderBuffer
+	{
+	public:
+		BpDebugLine* GetLines() { return &m_debugLines.front(); }
+		unsigned int GetLinesCount() { return m_debugLines.size(); }
+
+	public:
+		std::vector<BpDebugLine> m_debugLines;
+		std::vector<BpDebugSphere> m_debugSpheres;
+	};
+}
