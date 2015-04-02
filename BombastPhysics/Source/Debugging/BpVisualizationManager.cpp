ad7ce5df5f2ea5c3044c2d59e5f3a4d343ae35f2
Jason El-Massih <jel-massih@hotmail.com>
2015-04-02 13:10:35 -0400

    Fixed assert code to not throw warnings

--- BombastPhysics/Source/Debugging/BpVisualizationManager.cpp
+++ BombastPhysics/Source/Debugging/BpVisualizationManager.cpp
@@ -5,14 +5,14 @@ using namespace bPhysics;
 
 void BpVisualizationManager::SetVisualizationParameter(BpVisualizationParams parameter, float newScale)
 {
-	BP_ASSERT(parameter != BpVisualizationParams::VALUE_COUNT, "Attempting to set Invalid Visualualization Parameter");
+	BP_ASSERTf(parameter != BpVisualizationParams::VALUE_COUNT, "Attempting to set Invalid Visualualization Parameter");
 
 	m_visualizationParams[parameter] = newScale;
 }
 
 float BpVisualizationManager::GetVisualizationParameter(BpVisualizationParams parameter)
 {
-	BP_ASSERT(parameter != BpVisualizationParams::VALUE_COUNT, "Attempting to Get Invalid Visualualization Parameter");
+	BP_ASSERTf(parameter != BpVisualizationParams::VALUE_COUNT, "Attempting to Get Invalid Visualualization Parameter");
 
 	return m_visualizationParams[parameter];
 }
--- BombastPhysics/Source/Foundation/BpAssert.h
+++ BombastPhysics/Source/Foundation/BpAssert.h
@@ -1,8 +1,26 @@
 #pragma once
 
 #include <cassert>
+#include <cstdarg>
+#include <stdio.h>
 
 //Add actual implementation with an assertion handler
-#define BP_ASSERT(expr) assert(expr)
+#define BP_ASSERT(expr) \
+	do { \
+		if (!(expr)) \
+						{ \
+			assert(expr); \
+						} \
+			} while (0) \
 
-#define BP_ASSERT(expr, msg) assert(expr)
+#define BP_ASSERTf(expr, message, ...) \
+	do { \
+		if (!(expr)) \
+		{ \
+			char formattedMsg[1024]; \
+			va_list args; \
+			va_start(args, message); \
+			vsprintf_s(formattedMsg, sizeof(formattedMsg), message, args); \
+			assert(expr && formattedMsg); \
+		} \
+	} while (0) 
