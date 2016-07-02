37e5bb1044b19338e224f08bcdb7d96986f54a66
unknown <jel-massih@NYCL-JELMASSIH.rockstar.t2.corp>
2016-07-02 17:54:17 -0500

    Add script logging type

--- Engine/Source/Debugging/Logger.h
+++ Engine/Source/Debugging/Logger.h
@@ -44,8 +44,9 @@ namespace BELogger
 
 #define BE_INFO(str, ...) BE_LOG("INFO", str, ##__VA_ARGS__)
 
-#define BE_LOG_GRAPHICS(str, ...) BE_LOG("graphics", str, ##__VA_ARGS__)
-#define BE_LOG_PHYSICS(str, ...) BE_LOG("physics", str, ##__VA_ARGS__)
+#define BE_LOG_GRAPHICS(str, ...) BE_LOG("GRAPHICS", str, ##__VA_ARGS__)
+#define BE_LOG_PHYSICS(str, ...) BE_LOG("PHYSICS", str, ##__VA_ARGS__)
+#define BE_LOG_SCRIPT(str, ...) BE_LOG("SCRIPT", str, ##__VA_ARGS__)
 
 #define BE_LOG(tag, str, ...) BELogger::Log(tag, str, NULL, NULL, 0, ##__VA_ARGS__)
 
